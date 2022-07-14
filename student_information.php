3072a6b81c390dc252fbce32fc8142a4abb527d7
RyuujinnoKoen <pitpeschong@gmail.com>
2022-07-14 09:46:49 +0200

    half_working

--- student_information.php
+++ student_information.php
@@ -3,7 +3,7 @@
 <?php
 
 $conn = mysqli_connect('localhost', 'root', '', 'school');
-$query = "SELECT * FROM students";
+$query = "SELECT * FROM students INNER JOIN student_subjects ON students.id = student_subjects.student_id";
 $result = mysqli_query($conn, $query);
 $student = mysqli_fetch_assoc($result);
 
@@ -11,4 +11,6 @@ echo 'First Name: ' . $student['name'] . '<br>';
 echo 'Last Name: ' . $student['last_name'] . '<br>';
 echo 'Email: ' . $student['email'] . '<br>';
 echo 'Date of Birth: ' . $student['date_of_birth'] . '<br>';
-echo '<a href="student_update.php">Update</a>' . '<br>';
+echo 'Subject: ' . $student['subject'] . '<br>';
+echo 'Semester: ' . $student['semester'] . '<br>';
+echo "<a href='student_update.php?id={$student['id']}'>Update</a><br>";
